ef87f91cee97bbdeae6e012e7eceb2da7aeb661b
MbusyaNgesa <theengesa@gmail.com>
2024-08-11 14:54:18 +0300

    Classes and Inheritance

--- script.js
+++ script.js
@@ -31,13 +31,13 @@
 // const newArray = [1, 2, 3].map(() => true);
 
 //ES6 CRASH COURSE
-const person = {
-  name: "Ngesa",
-  walk() {
-    console.log(this);
-  },
-  talk() {},
-};
+// const person = {
+//   name: "Ngesa",
+//   walk() {
+//     console.log(this);
+//   },
+//   talk() {},
+// };
 
 //Use dot notation when you know ahead of time what property/method you're going to access
 // person.talk();
@@ -54,5 +54,101 @@ const person = {
 // console.log(walk);
 
 //Binding 'this'
-const walk = person.walk.bind(person);
-walk(); //calling a function as a standalone/outside the function using 'this'
+// const walk = person.walk.bind(person); //walk over here is an object
+// walk(); //calling a function as a standalone/outside the function using 'this'
+
+//ARROW FUNCTIONS
+//Old Method of using functions
+// const square = function(number){
+//   return number*number;
+// }
+
+// const square = (number) => number * number; Same code but Arrow Functions in ES6
+// console.log(square(5));
+
+// const jobs = [
+//   { id: 1, isActive: true },
+//   { id: 2, isActive: true },
+//   { id: 3, isActive: false },
+// ];
+
+// const activeJobs = jobs.filter(function (job) {
+//   return job.isActive;
+// }); //old JS
+
+// const activeJobs = jobs.filter((job) => job.isActive);
+// console.log(activeJobs);
+
+// const colors = ["red", "blue", "green"];
+
+// const items = colors.map((color) => `<li>${color}</li>`);
+// console.log(items);
+
+//OBJECT DESTRUCTURING
+// const address = {
+//   street: "dsfet",
+//   city: "",
+//   country: "",
+// };
+//Old JS
+// const street = address.street;
+// const city = address.city;
+
+//ES6
+// const { street, city } = address;
+// console.log(street);
+
+//SPREAD OPERATOR
+// const first = [1, 2, 3];
+// const second = [3, 4, 5];
+
+//Spread operator on Array
+// const combine = first.concat(second);
+//ES6
+// const combine = [...first, "fear", ...second];
+// console.log(combine);
+
+//Spread operator on Object
+// const person = { name: "Ngesa" };
+// const second = { age: 22 };
+
+// const combine = { ...person, ...second, location: "Nairobi" };
+// console.log(combine);
+// const clone = { ...person }; //Cloning can occur too
+// console.log(clone);
+
+//CLASSES
+// class Color {
+//   constructor(r, g, b) {
+//     // Assign the RGB values as a property of `this`.
+//     this.values = [r, g, b];
+//   }
+// }
+
+// const red = new Color(255, 0, 0);
+// console.log(red);
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  walk() {
+    console.log("walk");
+  }
+}
+// const person = new Person("Ngesa");
+// person.name;
+
+//INHERITANCE
+//We are inheritting from the class above
+class Teacher extends Person {
+  constructor(name, degree) {
+    super(name);
+    this.degree = degree;
+  }
+  teach() {
+    console.log("teach");
+  }
+}
+const teacher = new Teacher("Ngesa", "BScIT");
+teacher.teach;
